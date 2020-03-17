46e8d66dfe99da7c93f380b7c77e044b4f154c67
Chris Farmer <farmerc@udel.edu>
2020-03-17 11:17:31 -0400

    fixed error in continual testing

--- src/test/java/daos/JDBCTest.java
+++ src/test/java/daos/JDBCTest.java
@@ -14,8 +14,8 @@ public class JDBCTest {
 
     @Test
     public void getCardTest(){
-        String expected = dao.getCard("6767977022348089").getFirstName();
-        String actual = "Luella";
+        String expected = dao.getCard("3581107899214500").getFirstName();
+        String actual = "Clywd";
 
         Assert.assertEquals(actual, expected);
     }
@@ -23,7 +23,7 @@ public class JDBCTest {
     @Test
     public void findAllTest(){
         List allCards = dao.findAll();
-        int actual = 10;
+        int actual = 13;
         int expected = allCards.size();
         Assert.assertEquals(actual, expected);
     }
@@ -31,10 +31,10 @@ public class JDBCTest {
     @Test
     public void updateTest(){
         String actual = "A A RON";
-        dto = dao.getCard("6767977022348089");
+        dto = dao.getCard("3581107899214500");
         dto.setFirstName(actual);
         dao.update(dto);
-        String expected = dao.getCard("6767977022348089").getFirstName();
+        String expected = dao.getCard("3581107899214500").getFirstName();
 
         Assert.assertEquals(actual, expected);
     }
@@ -47,11 +47,14 @@ public class JDBCTest {
         String expected = dao.getCard("100").getType();
 
         Assert.assertEquals(expected, actual);
+        dao.delete("100");
     }
 
     @Test
     public void deleteTest(){
-        String cardNumber = "6767977022348089";
+        dto = new DTO(null, null, null, "20", null);
+        dao.create(dto);
+        String cardNumber = "20";
         Assert.assertNotNull(dao.getCard(cardNumber));
 
         dao.delete(cardNumber);
